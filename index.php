63bafc57ae576b25cb1aa1203955c01f13bae920
GCNB Ltda <gcnbltda@gmail.com>
2026-09-07 19:09:14 -0300

    Add index template

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php get_header(); ?><main class="litano-section"><div class="litano-container"><h1>LITANO Bullion</h1><?php if(have_posts()):while(have_posts()):the_post(); ?><article><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><?php the_excerpt(); ?></article><?php endwhile;endif; ?></div></main><?php get_footer(); ?>
